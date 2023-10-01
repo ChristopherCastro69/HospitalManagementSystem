37a7bd3417af7eed0fb3241f2de54a998463c1aa
Jethro Cenas <jethrocenas@gmail.com>
2023-10-01 14:20:30 +0800

    prescription-added

--- HMS/Prescription/models.py
+++ HMS/Prescription/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 class Prescription(models.Model):
     prescriptionID = models.AutoField(primary_key=True)
+    medicationName = models.CharField(max_length=30)
+    dosage = models.FloatField()
+    frequency = models.FloatField()
+    startDate = models.DateField()
+    endDate = models.DateField()
+    
 
