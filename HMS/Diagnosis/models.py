c788d6edac7650e77e517835c96a9d12f3480acd
Jethro Cenas <jethrocenas@gmail.com>
2023-10-02 13:32:00 +0800

    diagnosis-attributes added

--- HMS/Diagnosis/models.py
+++ HMS/Diagnosis/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from HMS.Patient.models import Patient
+from HMS.Doctor.models import Doctor
 # Create your models here.
 class Diagnosis(models.Model):
     diagnosisID = models.AutoField(primary_key=True)
+    diagnosisName = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    dateCreated = models.DateField()
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctorID = models.ForeignKey(Doctor, on_delete=models.CASCADE)
