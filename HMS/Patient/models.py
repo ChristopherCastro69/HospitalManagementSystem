a6a38faaf1c1bf7b98a486d047740b9cf453e786
VennDo1 <venndominic.delapena@cit.edu>
2023-10-02 13:19:58 +0800

    Patient/models

--- HMS/Patient/models.py
+++ HMS/Patient/models.py
@@ -5,3 +5,12 @@ from User.models import User
 # Create your models here.
 class Patient(User):
     patientID = models.AutoField(primary_key=True)
+    patientType = models.CharField(max_length=1, choices=(("R", "Resident"), ("O", "Outpatient")), default="R")
+
+
+class Resident(Patient):
+    roomNumber = models.CharField(max_length=5)
+
+
+class Outpatient(Patient):
+
